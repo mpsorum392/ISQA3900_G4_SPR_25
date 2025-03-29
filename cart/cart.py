5e8e45e45ecf77fc921ca2f85b3c92ec6473abe5
mpsorum392 <mpsorum392@hotmail.com>
2025-03-29 16:33:22 -0500

    updates
    updates

--- cart/cart.py
+++ cart/cart.py
@@ -38,8 +38,7 @@ class Cart(object):
         """
         Count all items in the cart.
         """
-        return sum(self,5)
-        #return sum(item['quantity'] for item in self.cart.values())
+        return sum(item['quantity'] for item in self.cart.values())
 
     def add(self, product, quantity=1, update_quantity=False):
         """
