bed2105c43361a8db2ef6603996c500152072df3
mpsorum392 <mpsorum392@hotmail.com>
2025-03-31 12:18:57 -0500

    updates for admin.py to show Orders info in admin console
    populated admin.py in the orders directory

--- products/models.py
+++ products/models.py
@@ -20,6 +20,7 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     image = models.ImageField(upload_to='products/', blank=True, null=True)  # requires Pillow
+    quantity = models.IntegerField(default=2)
 
     def __str__(self):
         return self.name
