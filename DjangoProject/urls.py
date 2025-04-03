c9f0a1e54295d527fcf6fb3666fd507ba5507d53
mpsorum392 <mpsorum392@hotmail.com>
2025-04-03 11:58:14 -0500

    updates for deploying to pythonanywhere
    updates for deploying to pythonanywhere

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -1,7 +1,11 @@
 #$DjangoProject/urls.py
 from django.contrib import admin
-from django.urls import path, include
+#added re_path as part of panywhere move
+from django.urls import path, include, re_path
 from accounts.views import home  # the home view we just created
+#added below two for pythonanywhere move
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('', home, name='home'),  # root URL uses our home view
@@ -12,5 +16,9 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
     path('orders/', include('orders.urls')),
+
+    #added below two for pythonanywhere move
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT}), #serve media files when deployed
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root':settings.STATIC_ROOT}), #serve static files when deployed
 ]
 
